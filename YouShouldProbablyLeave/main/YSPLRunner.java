df66084eafd2bfd63bb3f7f9fd5d8eec4a3c24aa
User <User@172.16.217.53>
2014-05-21 09:25:40 -0700

    Player Keys

--- YouShouldProbablyLeave/main/YSPLRunner.java
+++ YouShouldProbablyLeave/main/YSPLRunner.java
@@ -8,7 +8,6 @@ public class YSPLRunner {//this is a comment that quinn made.
 public static void main(String[] args) {
     ActorWorld world = new ActorWorld();
     PlayerActor player = new PlayerActor();
-    world.addKeyListener(player);
     world.add(player);
     world.show();
 }
