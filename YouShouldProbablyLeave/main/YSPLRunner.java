2ae6fd5eb94d6fb3a6528b236e1c4909a7db9d19
User <User@192.168.1.113>
2014-05-26 20:40:01 -0700

    Implementation of the GridMap, currently only basic internal repersentation of the grid.

--- YouShouldProbablyLeave/main/YSPLRunner.java
+++ YouShouldProbablyLeave/main/YSPLRunner.java
@@ -1,13 +1,15 @@
 package main;
 
-import player.PlayerActor;
+import info.gridworld.actor.Actor;
 import info.gridworld.actor.ActorWorld;
 import info.gridworld.actor.Bug;
+import player.PlayerActor;
+import world.grid.GridMap;
 
-public class YSPLRunner { 
+public class YSPLRunner {
 
 public static void main(String[] args) {
-    ActorWorld world = new ActorWorld();
+    ActorWorld world = new ActorWorld(new GridMap<Actor>(10, 10));
     YSPL.world = world;
     PlayerActor player = new PlayerActor();
     YSPL.player = player;
