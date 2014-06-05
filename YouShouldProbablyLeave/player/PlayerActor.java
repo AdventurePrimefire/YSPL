98b4500598e4c579b33585b40bf3bafe2358ee78
QuinnJMosher <Darkstaro59@gmail.com>
2014-06-05 09:16:12 -0700

    player stats imlementation part done.

--- YouShouldProbablyLeave/player/PlayerActor.java
+++ YouShouldProbablyLeave/player/PlayerActor.java
@@ -1,15 +1,40 @@
 package player;
 
 import java.awt.event.KeyEvent;
+
+import stats.CompleatStats;
 import main.YSPL;
 import info.gridworld.actor.Actor;
 import info.gridworld.grid.Location;
 
-public class PlayerActor extends Actor {
+public class PlayerActor extends Actor implements CompleatStats {
 private boolean running = false;
+
+private int level;
+
+private int maxHP;
+private int curHP;
+
+private int strength;
+private int defense;
+private int agility;
+
+private int nextExp;
+private int exp;
 
 public PlayerActor() {
     super();
+    this.level = 1;
+    
+    this.maxHP = 100;
+    this.curHP = 100;
+    
+    this.strength = 10;
+    this.defense = 5;
+    this.agility = 5;
+    
+    this.nextExp = 50;
+    this.exp = 0;
 }
 
 public void act() {}
@@ -62,5 +87,116 @@ private boolean moveInput() {
         }
     }
     return false;
+}
+
+@Override
+public int getMaxHp() {
+	return this.maxHP;
+}
+
+@Override
+public void setMaxHp(int hp) {
+	this.maxHP = hp;
+	
+}
+
+@Override
+public int getCurrentHp() {
+	return this.curHP;
+}
+
+@Override
+public void setCurrentHp(int hp) {
+	if (hp > this.maxHP) {
+		this.curHP = this.maxHP;
+	} else {
+		this.curHP = hp;
+	}
+	
+}
+
+@Override
+public int getStrength() {
+	return this.strength;
+}
+
+@Override
+public void setStrength(int str) {
+	this.strength = str;
+	
+}
+
+@Override
+public int getDefense() {
+	return this.defense;
+}
+
+@Override
+public void setDefense(int def) {
+	this.defense = def;
+	
+}
+
+@Override
+public int getAgility() {
+	return this.agility;
+}
+
+@Override
+public void setAgility(int agi) {
+	this.agility = agi;
+	
+}
+
+@Override
+public int getLevel() {
+	return this.level;
+}
+
+@Override
+public void setLevel(int lev) {
+	this.level = lev;
+	
+}
+
+@Override
+public void levelUp() {
+	this.level++;
+	
+	if (Math.random() > .2) {
+		this.maxHP += 5;
+	}
+	this.curHP = this.maxHP;
+	
+}
+
+@Override
+public int getNextExp() {
+	// TODO Auto-generated method stub
+	return 0;
+}
+
+@Override
+public void setNextExp(int exp) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public int getExp() {
+	// TODO Auto-generated method stub
+	return 0;
+}
+
+@Override
+public void setExp(int exp) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void addExp(int exp) {
+	// TODO Auto-generated method stub
+	
 }
 }
